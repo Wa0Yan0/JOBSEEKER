23b61e34b46af8a9f75293e62e7acb44f4c00394
wayan <1790484858@qq.com>
2021-11-08 17:30:31 +0800

    update company

--- src/main/java/com/atom/jobseeker/post/service/CompanyService.java
+++ src/main/java/com/atom/jobseeker/post/service/CompanyService.java
@@ -1,15 +1,31 @@
 package com.atom.jobseeker.post.service;
 
+import com.atom.jobseeker.common.utils.PageUtils;
 import com.atom.jobseeker.post.pojo.Company;
+
+import java.util.Map;
 
 /**
  * @author wayan
  */
 public interface CompanyService {
+
+
+    /**
+     * 查询带有分页和查询信息的数据
+     * @param params
+     * @return
+     */
+    PageUtils queryPage(Map<String, Object> params);
+
+
     /**
      * 通过id查询公司信息
      * @param id
      * @return
      */
     Company queryCompanyById(Long id);
+
+
+
 }
--- src/main/java/com/atom/jobseeker/post/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/atom/jobseeker/post/service/impl/CompanyServiceImpl.java
@@ -1,11 +1,16 @@
 package com.atom.jobseeker.post.service.impl;
 
+import com.atom.jobseeker.common.utils.IPage;
+import com.atom.jobseeker.common.utils.PageUtils;
 import com.atom.jobseeker.post.dao.CompanyDao;
 import com.atom.jobseeker.post.pojo.Company;
 import com.atom.jobseeker.post.service.CompanyService;
+import com.atom.jobseeker.post.vo.QueryVo;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -16,6 +21,17 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Resource
     private CompanyDao companyDao;
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params) {
+        QueryVo queryvo = new QueryVo(params);
+        IPage iPage = new IPage(params);
+        iPage.setTotalCount(queryvo.hasQuery() ? companyDao.selectCountWithQuery(queryvo) : companyDao.selectTotalCount());
+        List<Company> companyList = companyDao.selectListWithQuery(queryvo, iPage.getBegin(), iPage.getPageSize());
+        PageUtils pageUtils = new PageUtils(iPage);
+        pageUtils.setList(companyList);
+        return pageUtils;
+    }
 
     @Override
     public Company queryCompanyById(Long id) {
