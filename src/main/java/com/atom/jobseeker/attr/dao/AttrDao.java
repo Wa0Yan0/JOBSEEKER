a3eb7d9e9d52a6af5ed9602235f5951a8ceb08eb
wayanMac <coolboywayan@gmail.com>
2021-11-18 15:53:32 +0800

    modify base database

--- src/main/java/com/atom/jobseeker/attr/dao/AttrDao.java
+++ src/main/java/com/atom/jobseeker/attr/dao/AttrDao.java
@@ -1,8 +1,11 @@
 package com.atom.jobseeker.attr.dao;
 
 import com.atom.jobseeker.attr.pojo.City;
+import com.atom.jobseeker.attr.pojo.Location;
 import com.atom.jobseeker.attr.pojo.Major;
+import com.atom.jobseeker.attr.pojo.Region;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -22,4 +25,24 @@ public interface AttrDao {
      * @return
      */
     List<Major> selectMajorList();
+
+    /**
+     * 查询
+     * @param cityId
+     * @return
+     */
+    List<Location> selectLocationList(@Param("cityId") int cityId);
+
+    /**
+     * 插入城市信息
+     * @param region
+     * @return
+     */
+    int insert(@Param("region") Region region);
+
+    /**
+     * 批量插入地区信息
+     * @param regions
+     */
+    void bathInsert(@Param("regions") List<Region> regions);
 }
